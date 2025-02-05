52f31e87b5b807688508b0fd6d796c5d0e462896
Rick <rkessler@microsoft.com>
2025-02-04 22:05:27 -0800

    fix up.

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 from langchain_openai import ChatOpenAI
-from browser_use import Agent
-from dotenv import load_dotenv
+from browser_use import Agent, Browser
+from dotenv import load_dotenv, dotenv_values
+
 load_dotenv()
 
 import asyncio
@@ -8,16 +9,22 @@ import asyncio
 llm = ChatOpenAI(model="gpt-4o")
 
 async def main():
+    sensitive_data = {'x_name': dotenv_values("USERNAME"), 'x_password': dotenv_values("PASSWORD")}
+
+    browser = Browser()
     agent = Agent(
         task="""
-            1. visit wikipedia.com.
-            1. search tensors
-            1. summarize results of first article
+            1. go to stackoverflow.com
+            1. log in with username x_name and password x_password
         """,
         llm=llm,
+        browser=browser,
+        sensitive_data=sensitive_data
     )
-    result = await agent.run()
-    print(result)
-    print(result.screenshots())
+    await agent.run()
+
+    browser.close()
+    # print(result)
+    # print(result.screenshots())
 
 asyncio.run(main())
