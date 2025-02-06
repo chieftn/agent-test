44722ad7c54dfb6c1ba1698d8628291bcccd035c
Rick <rkessler@microsoft.com>
2025-02-06 07:45:38 -0800

    convert to azure open ai

--- agent.py
+++ agent.py
@@ -1,4 +1,4 @@
-from langchain_openai import ChatOpenAI
+from langchain_openai import AzureChatOpenAI
 from browser_use import Agent, Browser
 from dotenv import load_dotenv
 import os
@@ -7,7 +7,20 @@ load_dotenv()
 
 import asyncio
 
-llm = ChatOpenAI(model="gpt-4o")
+azure_openai_api_key = os.environ.get('AZURE_OPENAI_KEY')
+azure_openai_endpoint = os.environ.get('AZURE_OPENAI_ENDPOINT')
+azure_openai_api_model = os.environ.get('AZURE_OPENAI_MODEL')
+azure_openai_api_deployment = os.environ.get('AZURE_OPENAI_DEPLOYMENT')
+azure_openai_api_version = os.environ.get('AZURE_OPENAI_VERSION')
+
+# Initialize the Azure OpenAI client
+llm = AzureChatOpenAI(
+    model_name=azure_openai_api_model,
+    openai_api_key=azure_openai_api_key,
+    azure_endpoint=azure_openai_endpoint,
+    deployment_name=azure_openai_api_deployment,
+    api_version=azure_openai_api_version
+)
 
 async def main():
 
