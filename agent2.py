5a0b0386757ec4398cbc22a6229408c5082a030b
Maxwell Talley <matalley@microsoft.com>
2025-02-07 23:36:44 +0000

    minor updates

--- agent2.py
+++ agent2.py
@@ -37,7 +37,8 @@ llm = AzureChatOpenAI(
 async def main():
     browser = Browser(
         config=BrowserConfig(
-            headless=True
+            headless=True,
+            # chrome_instance_path="/snap/bin/chromium"
         )
     )
 
@@ -51,50 +52,40 @@ async def main():
     
     print(f'Got test user password {secretName} from keyvault.')
 
-    async with async_playwright() as p:
-        # playwrightBrowser = await p.chromium.launch(headless=False)
-        # context
-        browserContext = await browser.new_context()
-        page = await browserContext.new_page()
-        await page.goto('https://aka.ms/doe-ppe')
+    async with await browser.new_context(
+        config=BrowserContextConfig(
+            trace_path='./tmp/traces/',
+            save_recording_path='./tmp/traces/',
+            cookies_file='./tmp/cookies.json'
+        )
+    ) as context:
+        # agent = Agent(
+        #     task='Go to hackernews, then go to apple.com and return all titles of open tabs',
+        #     llm=llm,
+        #     browser_context=context,
+        # )
+        # await agent.run()
 
-        await page.getByPlaceholder('Email or phone').fill(e2eUserEmail)
-        await page.getByRole('button', name='Next').click()
+        sensitive_data = {
+            'e2eUserEmail': e2eUserEmail,
+            'e2eUserPassword': e2eUserPassword
+        }
+        task = """
+            Navigate to https://aka.ms/doe-ppe and login with the username e2eUserEmail and password e2eUserPassword. 
+            Once the login flow is complete, confirm the text Welcome to Azure IoT Operations is visible. 
+            If you get into a loop where you can't reauthorize, click the three dots to logout and try logging in again.
+            End the test if you run into the loop more than 3 times, after trying to clear site data.
+        """
+            # If logging out doesn't work after the first try, clear the browser cookies and cache and try again.
 
-        
+        agent2 = Agent(
+            task=task,
+            llm=llm,
+            browser_context=context,
+            sensitive_data=sensitive_data
+        )
 
-
-    # async with await browser.new_context(
-    #     config=BrowserContextConfig(trace_path='./tmp/traces/')
-    # ) as context:
-    #     # agent = Agent(
-    #     #     task='Go to hackernews, then go to apple.com and return all titles of open tabs',
-    #     #     llm=llm,
-    #     #     browser_context=context,
-    #     # )
-    #     # await agent.run()
-
-    #     sensitive_data = {
-    #         'e2eUserEmail': e2eUserEmail,
-    #         'e2eUserPassword': e2eUserPassword
-    #     }
-    #     task = """
-    #         Navigate to https://aka.ms/doe-ppe and login with the username e2eUserEmail and password e2eUserPassword. 
-    #         Once the login flow is complete, confirm the text Welcome to Azure IoT Operations is visible. 
-    #         If you see a message saying you need to reauthorize, open the browser console and watch for console errors or network errors. Continue with next instructions.
-    #         If you get into a loop where you can't reauthorize, click the three dots to logout and try logging in again.
-    #         If logging out doesn't work after the first try, clear the browser cookies and cache and try again.
-    #         End the test if you run into the loop more than 3 times, after trying to clear site data.
-    #     """
-
-    #     agent2 = Agent(
-    #         task=task,
-    #         llm=llm,
-    #         browser_context=context,
-    #         sensitive_data=sensitive_data
-    #     )
-
-    #     await agent2.run()
+        await agent2.run()
 
     await browser.close()
     
